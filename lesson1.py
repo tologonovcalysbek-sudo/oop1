dccf6d9ba8877be8ef213322eecf740dcfc7d1b0
Kalys <tologonovcalysbek@gmail.com>
2026-03-30 21:03:29 +0600

    first

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+class Kaka:
+    # Конструктор класса
+    def __init__(self, name, hp, lvl):
+        # Атрибуты
+        self.name_hero = name
+        self.hp_hero = hp
+        self.lvl_hero = lvl
+    # Объект класса
+    def action(self):
+        return f"{self.name_hero}Hero base action"
+
+
+#Объкт/ Экземпляр на основе класса
+kirito = Kaka('Kirito', 1000, 100)
+asuna = Kaka('asuna', 1000, 100)
+
+
+
+
+
+
